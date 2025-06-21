65041eb6dee0c15ceb553a6285ab56f7e7b00034
matheoleger <windersmail@gmail.com>
2025-06-21 20:39:05 +0200

    setup(pwa): init pwa project (vite, react, router, etc...)

---
+++ apps/pwa/src/routes/home.tsx
@@ -0,0 +1,5 @@
+import { Welcome } from '@/features/welcome/welcome'
+
+export default function Home() {
+  return <Welcome />
+}
---
+++ apps/pwa/vite.config.ts
@@ -0,0 +1,46 @@
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+})
