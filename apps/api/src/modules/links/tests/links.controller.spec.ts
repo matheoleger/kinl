e7d2101cd52a1eec60799a002c943cb7f9190632
matheoleger <windersmail@gmail.com>
2025-07-05 14:54:27 +0200

    setup(ci): added ci github actions + fix test with prisma

--- apps/api/src/modules/links/tests/links.controller.spec.ts
+++ apps/api/src/modules/links/tests/links.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { LinksController } from '../links.controller';
+import { LinksService } from '../links.service';
 
 describe('linksController', () => {
   let controller: LinksController;
@@ -7,6 +9,7 @@ describe('linksController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LinksController],
+      providers: [LinksService, { provide: PrismaService, useValue: {} }],
     }).compile();
 
     controller = module.get<LinksController>(LinksController);
