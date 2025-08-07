22a01ef37adf6b4299914b67fee38ccdaa9c7f30
matheoleger <windersmail@gmail.com>
2025-08-07 11:17:55 +0200

    feat(search): implement first functionnal search for links and tags

--- apps/pwa/src/features/tags/components/tags-list.tsx
+++ apps/pwa/src/features/tags/components/tags-list.tsx
@@ -1,13 +1,24 @@
+import { useMemo } from 'react';
+import { useTopBar } from '@/contexts/topbar/topbar-provider';
 import { useTags } from '../hooks/tags';
 import { TagCard } from './tag-card';
 
 export function TagsList() {
+  const { search } = useTopBar();
   const { data: tags } = useTags();
+
+  const filteredTags = useMemo(() => {
+    if (!search) {
+      return tags;
+    }
+
+    return tags?.filter(tag => tag.name.toLowerCase().includes(search.toLowerCase()));
+  }, [search, tags]);
 
   return (
     <ul className="grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] gap-4 justify-items-center px-6">
       {
-        tags?.map(tag => (
+        filteredTags?.map(tag => (
           <li key={tag.id} className="w-full">
             <TagCard tag={tag} />
           </li>
